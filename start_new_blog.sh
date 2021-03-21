873fced68860a04ef0dde3bf5ee2e585e857cdc5
KG <c@jkl.gg>
2021-03-20 21:54:34 -0700

    add a cleanup script to quickly create blog

---
+++ start_new_blog.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
+GRAY='\033[1;30m'
+NC='\033[0m' # No Color
+
+printf "\n\n${YELLOW}---- remove files already supplied by gem${NC}\n"
+
+rm _sass/base.scss
+rm _sass/code.css
+rm _sass/main.scss
+rm _sass/mixins.scss
+rm _sass/theme.scss
+
+rm -rf assets/img/*
+rm -rf assets/js
+
+rm -rf _layouts
+rm -rf _includes
+
+printf "\n\n${GRAY}---- removing gem development files${NC}\n"
+rm henry-jekyll.gemspec
+rm LICENSE.txt
+rm README.md
+rm DEVELOPMENT.md
+rm .ruby-version
+rm Gemfile
+rm Gemfile.lock
+
+printf "\n\n${GRAY}---- Cleanup${NC}\n"
+rm *.gem
+rm -rf .bundle
+rm -rf .idea
+rm -rf .jekyll-cache
+rm -rf .jekyll-metadata
+rm -rf .sass-cache
+rm -rf _site
+rm -rf vendor
+
+
+printf "\n\n${YELLOW}---- cleaning up git config${NC}\n"
+rm -rf .idea
+rm -rf .git
+git init
+git add .
+git commit -m "first commit (jekyll blog with henry theme)"
+
+
+printf "\n\n${PURPLE}All done!  ${NC}\n"
